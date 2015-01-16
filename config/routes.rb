12cc62c341f3caac82cc832f0917e7c4bcc80c8a
Nathan R <nathan19@hotmail.com>
2015-01-15 18:04:02 -0800

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root 'topics#index' 
+  resources :topics do 
+    member do 
+      post 'upvote' 
+      post 'downvote'
+    end 
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
